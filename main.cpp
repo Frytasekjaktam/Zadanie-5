222ce309c33e682d127f05655afc2ce4b73cbc7e
Frytasekjaktam <92473596+Frytasekjaktam@users.noreply.github.com>
2021-11-08 23:36:53 +0100

    Wczytywanie z pliku i komentarze

--- main.cpp
+++ main.cpp
@@ -5,17 +5,18 @@
 using namespace std;
 
 int ile = 0;
-const string fileName = "output.txt";
+const string fileName = "output.txt"; // nazwa pliku do odczytu, ktory wygeneruje program
 
+//FUNKCJA TWORZACA I WYPELNIAJACA TABLICE
 int *stworzTablice()
 {
     cout <<"Podaj rozmiar tablicy: ";
      cin >> ile;
-    while(cin.fail() || ile <= 0)
+    while(cin.fail() || ile <= 1) //petla ktora sprawdza czy wprowadzone dane sa liczbami wiekszymi lub rownymi jeden
     {
-       cout << "Wprowadz poprawna wartosc." << endl;
-        cin.clear();
-        cin.ignore(256,'\n');
+       cout << "Niepoprawna wartosc " << endl;
+        cin.clear(); // czysci flage bledu
+        cin.ignore(100,'\n'); //ignoruje niepoprawne dane czyli max 100 znakow az do spotkanie nowej linii
         cin >> ile;
     }
 
@@ -25,58 +26,81 @@ int *stworzTablice()
     {
         cout << "Podaj element nr." << i+1 << ": ";
          cin >> tab[i];
-        while( cin.fail() )
+        while( cin.fail() )  // petla ktora rowniez sprawdza poprawnosc wprowadzonych danych (tylko czy jest liczba)
         {
             cout << "Wprowadz poprawna wartosc." << endl;
              cin.clear();
-             cin.ignore(256,'\n');
+             cin.ignore(100,'\n');
              cin >> tab[i];
         }
     }
 
-    sort( tab, tab + ile );
+    sort( tab, tab + ile ); //sortuje wpraowadzone dane od najmniejszego do najwiekszego
     return tab;
 
 }
 
+//FUNKCJA OBLICZAJACA ILOCZYNY Z TABLICY
 void calcProduct( int tab[], int ile )
 {
-    int  counter = 0;
+    ofstream file; //zmienna plik
+    file.open (fileName); //otwiera plik
+
+    int  counter = 0;  //zmienna licznik ktora wykorzystana bedzie do petli sprawdzajacej i zamieniajacej wartosc maksymalna z tablicy 
     int* Iloczyny = new int [ile - 1]; //tworzymy tablice iloczynow
     Iloczyny[0] = tab[0] * tab[1];
 
-    cout<<"\n\nIloczyny\n\n";
+    file << "\n\nIloczyny\n\n";
 
-    for(int i=0; i<ile-1; i++)
+    for(int i=0; i<ile-1; i++) //petla wypisujaca iloczyny dwoch sasiednich liczb (wczesniej posortowanych)
     {
         Iloczyny[i] = tab[i] * tab[i+1];
-           cout << i+1 << ". " << Iloczyny[i] << endl;
+           file << i+1 << ". " << Iloczyny[i] << endl;
         counter += 1;
     }
     
     int maks = Iloczyny[0];
 
-for(int j=0; j<counter; j++)
+for(int j=0; j<counter; j++) //petla przechodzaca przez kolejne iloczyny, jezeli napotka wiekszy iloczyn to zamienia go do zmiennej maks
     {
         if( Iloczyny[j+1] > Iloczyny[j] )
         {
             maks = Iloczyny[j+1];
         }
     }
-    cout << "Najwiekszy iloraz: " << maks << endl;
-    cout << "Czynniki generujace maksymalny iloczyn to pary: ";
+    file << "\nNajwiekszy iloraz: " << maks << endl;
+    file << "Czynniki generujace maksymalny iloczyn to pary: ";
     
-    for( int i=0; i< ile - 1; i++ )
+    for( int i=0; i< ile - 1; i++ )  //petla wypisuje pary liczb
     {
         if( tab[i] * tab[i+1] == maks )
         {
-            cout << "[ " << tab[i] << ", " << tab[i+1] <<" ]  ";
+            file << "[ " << tab[i] << ", " << tab[i+1] <<" ]  ";
         }
     }
+    file << "\n";
 }
+
+
+// FUNKCJA GLOWNA
 int main()
 {
-    int* tablica = stworzTablice();
-    calcProduct( tablica, ile );
-    delete [] tablica;
+    int* tablica = stworzTablice(); // tworzymy tablice dynamiczna na podstawie wprowadzonych danych i przy okazji sortujemy je w kolejnosci rosnacej
+    calcProduct( tablica, ile ); //odwolujemy sie do funkcji z dwoma argumentami
+    delete [] tablica; //zwalniamy pamiec usuwajac tablice dynamiczna 
+
+// WYPISYWANIE PLIKU NA EKRAN
+    ifstream outcome(fileName); // wypisuje dane na ekran, jesli nastapi blad to wyswietli stosowny komunikat
+
+    if ( outcome.is_open() )
+    {
+        cout << outcome.rdbuf();
+        outcome.close();
+    }
+    else
+    {
+        cout << "Blad przy otwieraniu pliku: " << fileName;
+    }
+
+    return 0;
 }
